76fc3328e69af7c80a7ef7ad2a58bff1f117b61d
cwilko <chris.wilkinson@uk.ibm.com>
2020-11-02 22:14:17 +0000

    Linter formatting & Test/Env updates

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-""" Hello World app for deploying Python functions as APIs on Bluemix """ 
-# Always prefer setuptools over distutils 
-from setuptools import setup 
-from codecs import open 
-from os import path 
-here = path.abspath(path.dirname(__file__)) 
+# Always prefer setuptools over distutils
+from setuptools import setup
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read() 
+    long_description = f.read()
 
-setup( 
-	name='marketinsights-model-api', 
-	version='1.0.0', 
-	description='Train, Deploy, and Score models', long_description=long_description, 
-	url='https://github.com/cwilko/marketinsights-ml', license='MIT' 
+setup(
+    name='marketinsights-model-api',
+    version='1.0.0',
+    description='Train, Deploy, and Score models',
+    long_description=long_description,
+    url='https://github.com/cwilko/marketinsights-ml',
+    license='MIT'
 )
--- tests/unittest/test_MLModels.py
+++ tests/unittest/test_MLModels.py
@@ -15,16 +15,16 @@ from quantutils.model.ml import Model
 
 MODEL_ID = "3a491b1a-8af6-416d-aa14-f812cbd660bb"
 
-DATASET_ID1 = "4234f0f1b6fcc17f6458696a6cdf5101" # DOW
-DATASET_ID2 = "3231bbe5eb2ab84eb54c9b64a8dcea55" # SPY
+DATASET_ID1 = "4234f0f1b6fcc17f6458696a6cdf5101"  # DOW
+DATASET_ID2 = "3231bbe5eb2ab84eb54c9b64a8dcea55"  # SPY
 
 TRAINING_RUN = {
-        "model_id": MODEL_ID,
-        "datasets": [
-            DATASET_ID1,
-            DATASET_ID2
-        ]
-    }
+    "model_id": MODEL_ID,
+    "datasets": [
+        DATASET_ID1,
+        DATASET_ID2
+    ]
+}
 
 COS_BUCKET = "marketinsights-weights"
 NUM_FEATURES = (2 * 4) + 1
@@ -33,171 +33,169 @@ NUM_LABELS = 1
 cred = CredentialsStore()
 cos = CloudObjectStore(cred)
 
+
 class MLModelTestCase(unittest.TestCase):
 
-	
+    def setUp(self):
 
-	def setUp(self):
+        #
+        # Get dataset from MI API #
+        #
 
-		#
-		# Get dataset from MI API #
-		#
+        print("Loading data...")
 
-		print("Loading data...")
-		
-		mi = MarketInsights(cred)
-		fun = Functions(cred)
-		self.miassembly = MIAssembly(mi, fun)
+        mi = MarketInsights(cred)
+        fun = Functions(cred)
+        self.miassembly = MIAssembly(mi, fun)
 
-		TRAINING_RUN["id"] = cos.generateKey([str(TRAINING_RUN["datasets"]), str(TRAINING_RUN["model_id"])])
-		
-		mi.put_training_run(TRAINING_RUN)
+        TRAINING_RUN["id"] = cos.generateKey([str(TRAINING_RUN["datasets"]), str(TRAINING_RUN["model_id"])])
 
-		self.CONFIG = mi.get_model(MODEL_ID)
-		TRN_CNF = self.CONFIG["training"]	
+        mi.put_training_run(TRAINING_RUN)
 
-		print("Creating model...")
-		# Create ML model
-		self.ffnn = Model(NUM_FEATURES, NUM_LABELS, self.CONFIG)		
+        self.CONFIG = mi.get_model(MODEL_ID)
+        TRN_CNF = self.CONFIG["training"]
 
-		mkt1, mkt1_desc = mi.get_dataset_by_id(DATASET_ID1)
-		mkt2, mkt2_desc = mi.get_dataset_by_id(DATASET_ID2)
+        print("Creating model...")
+        # Create ML model
+        self.ffnn = Model(NUM_FEATURES, NUM_LABELS, self.CONFIG)
 
-		# Crop training dates
-		if "training_end_date" in TRN_CNF:
-		    mkt1 = mkt1[TRN_CNF["training_start_date"]:TRN_CNF["training_end_date"]]
-		    mkt2 = mkt2[TRN_CNF["training_start_date"]:TRN_CNF["training_end_date"]]
+        mkt1, mkt1_desc = mi.get_dataset_by_id(DATASET_ID1)
+        mkt2, mkt2_desc = mi.get_dataset_by_id(DATASET_ID2)
 
-		# Interleave (part of the "added insight" for this model)
-		self.mkt1, self.mkt2, self.isect = ppl.intersect(mkt1,mkt2)
-		self.dataset = ppl.interleave(self.mkt1,self.mkt2)
+        # Crop training dates
+        if "training_end_date" in TRN_CNF:
+            mkt1 = mkt1[TRN_CNF["training_start_date"]:TRN_CNF["training_end_date"]]
+            mkt2 = mkt2[TRN_CNF["training_start_date"]:TRN_CNF["training_end_date"]]
 
-		self.TRAINING_SET_SIZE = TRN_CNF["training_window_size"]
-		self.TEST_SET_SIZE = len(self.dataset) - self.TRAINING_SET_SIZE
-		self.WINDOW_SIZE = self.TRAINING_SET_SIZE
+        # Interleave (part of the "added insight" for this model)
+        self.mkt1, self.mkt2, self.isect = ppl.intersect(mkt1, mkt2)
+        self.dataset = ppl.interleave(self.mkt1, self.mkt2)
 
-		_ , self.test_y = ppl.splitCol(self.dataset[self.TRAINING_SET_SIZE:], NUM_FEATURES)
+        self.TRAINING_SET_SIZE = TRN_CNF["training_window_size"]
+        self.TEST_SET_SIZE = len(self.dataset) - self.TRAINING_SET_SIZE
+        self.WINDOW_SIZE = self.TRAINING_SET_SIZE
 
-	def testFFNN_BootstrapTrain(self):
+        _, self.test_y = ppl.splitCol(self.dataset[self.TRAINING_SET_SIZE:], NUM_FEATURES)
 
-		###############
-		# Test Training
-		###############
+    def testFFNN_BootstrapTrain(self):
 
-		print("testFFNN_BootstrapTrain")
+        ###############
+        # Test Training
+        ###############
 
-		TRN_CNF = self.CONFIG['training']
-		print("Training", end='')
-		
-		results = mlutils.bootstrapTrain(self.ffnn, self.dataset[:self.TRAINING_SET_SIZE], self.dataset[self.TRAINING_SET_SIZE:], TRN_CNF['lamda'], TRN_CNF['iterations'], TRN_CNF['threshold'], False)
-		predictions =  np.nanmean(results["test_predictions"], axis=0)
-		result = mlutils.evaluate(ppl.onehot(predictions), ppl.onehot(self.test_y), .0)
-		print(result)
-		
-		print("".join(["Received : ", str(result)]))
-		print("Expected : 0.48139533")
-		self.assertTrue(np.allclose(result, 0.48139533)) # Local results
+        print("testFFNN_BootstrapTrain")
 
-		##################
-		# Test weights API
-		##################
+        TRN_CNF = self.CONFIG['training']
+        print("Training", end='')
 
-		# Save weights to Cloud Object Store
-		newWeights = pd.DataFrame(results["weights"])
-		newWeights.insert(0,'timestamp', [self.isect[self.TRAINING_SET_SIZE//2].value // 10**9] * len(newWeights))
-		cos.put_csv(COS_BUCKET, TRAINING_RUN["id"], newWeights)
+        results = mlutils.bootstrapTrain(self.ffnn, self.dataset[:self.TRAINING_SET_SIZE], self.dataset[self.TRAINING_SET_SIZE:], TRN_CNF['lamda'], TRN_CNF['iterations'], TRN_CNF['threshold'], False)
+        predictions = np.nanmean(results["test_predictions"], axis=0)
+        result = mlutils.evaluate(ppl.onehot(predictions), ppl.onehot(self.test_y), .0)
+        print(result)
 
-		loadedWeights = cos.get_csv(COS_BUCKET, TRAINING_RUN["id"])
-		self.assertTrue(np.allclose(newWeights.values, loadedWeights.values))
+        print("".join(["Received : ", str(result)]))
+        print("Expected : 0.47906977")
+        self.assertTrue(np.allclose(result, 0.47906977))  # Local results
 
-		#####################################
-		# Test prediction from loaded weights
-		#####################################
+        ##################
+        # Test weights API
+        ##################
 
-		dataset = self.mkt1.iloc[:,:-NUM_LABELS][-50:]
-		timestamps = dataset.index.astype(np.int64) // 10**9
-		dataset = dataset.reset_index(drop=True)
-		newPredictions = self.predict(timestamps, dataset, loadedWeights)
-		self.assertTrue(np.allclose(newPredictions, results["test_predictions"][0][-100:][::2]))
+        # Save weights to Cloud Object Store
+        newWeights = pd.DataFrame(results["weights"])
+        newWeights.insert(0, 'timestamp', [self.isect[self.TRAINING_SET_SIZE // 2].value // 10**9] * len(newWeights))
+        cos.put_csv(COS_BUCKET, TRAINING_RUN["id"], newWeights)
 
+        loadedWeights = cos.get_csv(COS_BUCKET, TRAINING_RUN["id"])
+        self.assertTrue(np.allclose(newWeights.values, loadedWeights.values))
 
-		#####################################
-		# Test prediction from Assembly
-		#####################################
-		dataset = self.mkt1.iloc[:,:-NUM_LABELS][-50:]
-		assemblyPredictions = self.miassembly.get_predictions_with_dataset(dataset, TRAINING_RUN["id"])
-		self.assertTrue(np.allclose(newPredictions.flatten(), assemblyPredictions[0].values.flatten(), rtol=1e-03))
+        #####################################
+        # Test prediction from loaded weights
+        #####################################
 
-	def testFFNN_BoostingTrain(self):
+        dataset = self.mkt1.iloc[:, :-NUM_LABELS][-50:]
+        timestamps = dataset.index.astype(np.int64) // 10**9
+        dataset = dataset.reset_index(drop=True)
+        newPredictions = self.predict(timestamps, dataset, loadedWeights)
+        self.assertTrue(np.allclose(newPredictions, results["test_predictions"][0][-100:][::2]))
 
-		###############
-		# Test Training
-		###############
+        #####################################
+        # Test prediction from Assembly
+        #####################################
+        dataset = self.mkt1.iloc[:, :-NUM_LABELS][-50:]
+        assemblyPredictions = self.miassembly.get_predictions_with_dataset(dataset, TRAINING_RUN["id"])
+        self.assertTrue(np.allclose(newPredictions.flatten(), assemblyPredictions[0].values.flatten(), rtol=1e-03))
 
-		print("testFFNN_BoostingTrain")
+    def testFFNN_BoostingTrain(self):
 
-		TRN_CNF = self.CONFIG['training']
-		print("Training", end='')
-		
-		results = mlutils.boostingTrain(self.ffnn, self.dataset[:self.TRAINING_SET_SIZE], self.dataset[self.TRAINING_SET_SIZE:], TRN_CNF['lamda'], TRN_CNF['iterations'], False)
-		predictions =  np.nanmean(results["test_predictions"], axis=0)
-		result = mlutils.evaluate(ppl.onehot(predictions), ppl.onehot(self.test_y), .0)
+        ###############
+        # Test Training
+        ###############
 
-		print("".join(["Received : ", str(result)]))
-		print("Expected : 0.47674417")
-		self.assertTrue(np.allclose(result, 0.47674417)) # Local results
+        print("testFFNN_BoostingTrain")
 
-		##################
-		# Test weights API
-		##################
+        TRN_CNF = self.CONFIG['training']
+        print("Training", end='')
 
-		# Save weights to Cloud Object Store
-		newWeights = pd.DataFrame(results["weights"])
-		newWeights.insert(0,'timestamp', [self.isect[self.TRAINING_SET_SIZE//2].value // 10**9] * len(newWeights))
-		cos.put_csv(COS_BUCKET, TRAINING_RUN["id"], newWeights)
+        results = mlutils.boostingTrain(self.ffnn, self.dataset[:self.TRAINING_SET_SIZE], self.dataset[self.TRAINING_SET_SIZE:], TRN_CNF['lamda'], TRN_CNF['iterations'], False)
+        predictions = np.nanmean(results["test_predictions"], axis=0)
+        result = mlutils.evaluate(ppl.onehot(predictions), ppl.onehot(self.test_y), .0)
 
-		loadedWeights = cos.get_csv(COS_BUCKET, TRAINING_RUN["id"])
-		self.assertTrue(np.allclose(newWeights.values, loadedWeights.values))
+        print("".join(["Received : ", str(result)]))
+        print("Expected : 0.51627904")
+        self.assertTrue(np.allclose(result, 0.51627904))  # Local results
 
-		#####################################
-		# Test prediction from loaded weights
-		#####################################
+        ##################
+        # Test weights API
+        ##################
 
-		dataset = self.mkt2.iloc[:,:-NUM_LABELS][-50:]
-		timestamps = dataset.index.astype(np.int64) // 10**9
-		dataset = dataset.reset_index(drop=True)
-		newPredictions = self.predict(timestamps, dataset, loadedWeights)
-		self.assertTrue(np.allclose(newPredictions, results["test_predictions"][0][-100:][1::2]))
+        # Save weights to Cloud Object Store
+        newWeights = pd.DataFrame(results["weights"])
+        newWeights.insert(0, 'timestamp', [self.isect[self.TRAINING_SET_SIZE // 2].value // 10**9] * len(newWeights))
+        cos.put_csv(COS_BUCKET, TRAINING_RUN["id"], newWeights)
 
-		#####################################
-		# Test prediction from Assembly
-		#####################################
-		dataset = self.mkt2.iloc[:,:-NUM_LABELS][-50:]
-		assemblyPredictions = self.miassembly.get_predictions_with_dataset(dataset, TRAINING_RUN["id"])
-		self.assertTrue(np.allclose(newPredictions.flatten(), assemblyPredictions[0].values.flatten(), rtol=1e-03))
+        loadedWeights = cos.get_csv(COS_BUCKET, TRAINING_RUN["id"])
+        self.assertTrue(np.allclose(newWeights.values, loadedWeights.values))
 
-	# Function to take dates, dataset info for those dates
-	def predict(self, timestamps, dataset, weights=None):
+        #####################################
+        # Test prediction from loaded weights
+        #####################################
 
-	    # Load timestamps from weights db (or load all weights data)
-	    if (weights is None):
-	    	weights = cos.get_csv(COS_BUCKET, TRAINING_RUN["id"])
-	    wPeriods = weights["timestamp"].values
+        dataset = self.mkt2.iloc[:, :-NUM_LABELS][-50:]
+        timestamps = dataset.index.astype(np.int64) // 10**9
+        dataset = dataset.reset_index(drop=True)
+        newPredictions = self.predict(timestamps, dataset, loadedWeights)
+        self.assertTrue(np.allclose(newPredictions, results["test_predictions"][0][-100:][1::2]))
 
-	    # x = for each dataset timestamp, match latest available weight timestamp
-	    latestPeriods = np.zeros(len(timestamps)) 
-	    uniqueWPeriods = np.unique(wPeriods)  # q
-	    mask = timestamps>=np.min(uniqueWPeriods)
-	    latestPeriods[mask] = [uniqueWPeriods[uniqueWPeriods<=s][-1] for s in timestamps[mask]]
+        #####################################
+        # Test prediction from Assembly
+        #####################################
+        dataset = self.mkt2.iloc[:, :-NUM_LABELS][-50:]
+        assemblyPredictions = self.miassembly.get_predictions_with_dataset(dataset, TRAINING_RUN["id"])
+        self.assertTrue(np.allclose(newPredictions.flatten(), assemblyPredictions[0].values.flatten(), rtol=1e-03))
 
-	    # for each non-duplicate timestamp in x, load weights into model for that timestamp
-	    results = np.empty((len(dataset), NUM_LABELS))
-	    for x in np.unique(latestPeriods):
-	        # run dataset entries matching that timestamp through model, save results against original timestamps
-	        mask = latestPeriods==x
-	        results[mask] = np.nanmean(self.ffnn.predict(weights[wPeriods==x].values[:,1:], dataset[mask]), axis=0)
-	    
-	    return results    
+    # Function to take dates, dataset info for those dates
+    def predict(self, timestamps, dataset, weights=None):
+
+        # Load timestamps from weights db (or load all weights data)
+        if (weights is None):
+            weights = cos.get_csv(COS_BUCKET, TRAINING_RUN["id"])
+        wPeriods = weights["timestamp"].values
+
+        # x = for each dataset timestamp, match latest available weight timestamp
+        latestPeriods = np.zeros(len(timestamps))
+        uniqueWPeriods = np.unique(wPeriods)  # q
+        mask = timestamps >= np.min(uniqueWPeriods)
+        latestPeriods[mask] = [uniqueWPeriods[uniqueWPeriods <= s][-1] for s in timestamps[mask]]
+
+        # for each non-duplicate timestamp in x, load weights into model for that timestamp
+        results = np.empty((len(dataset), NUM_LABELS))
+        for x in np.unique(latestPeriods):
+            # run dataset entries matching that timestamp through model, save results against original timestamps
+            mask = latestPeriods == x
+            results[mask] = np.nanmean(self.ffnn.predict(weights[wPeriods == x].values[:, 1:], dataset[mask]), axis=0)
+
+        return results
 
 if __name__ == '__main__':
     unittest.main()
